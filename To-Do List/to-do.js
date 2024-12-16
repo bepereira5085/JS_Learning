0fe06e6d4141be73f5b7d82562037984904e1293
Bernardo Pereira Candido <164104265+bepereira5085@users.noreply.github.com>
2024-12-15 21:30:25 -0300

    Add files via upload

---
+++ To-Do List/to-do.js
@@ -0,0 +1,71 @@
+let taskList
+
+/* CARREGAR A LISTA */
+
+function loadTasks() {
+    let list = localStorage.getItem('taskList')
+    taskList = list ? JSON.parse(list) : []
+    updateList()
+}
+
+
+/* CONSTRÓI A LISTA DE TAREFA */
+
+function updateList() {
+
+    let tasklist = document.getElementById('tasklist')
+    let initial_message = document.getElementById('tasklist').innerHTML
+    tasklist.innerHTML = ''
+
+    if (taskList.length > 0) {
+        
+        let newOl = document.createElement('ol')
+        tasklist.append(newOl)
+
+        taskList.forEach((task, index) => {
+            let newLi = document.createElement('li')
+            let icon_delete = document.createElement('span')
+            let position_number = document.createElement('span')
+            let task_listened = document.createElement('span')
+            
+            position_number.innerHTML = `${index + 1}°`
+            task_listened.innerHTML = task
+
+            icon_delete.classList = 'material-icons'
+            icon_delete.innerHTML = 'delete'
+            icon_delete.onclick = () => removeTask(index)
+
+            newLi.append( position_number, task_listened, icon_delete)
+            newOl.append(newLi)
+        });
+        
+    } else {
+        tasklist.innerHTML = initial_message
+    }
+    console.log(taskList)
+}
+
+
+/* ADICIONAR TAREFA */
+
+function addTask(event){
+    event.preventDefault()
+    let task = document.getElementById('task')
+    if (task.value == "") {
+        window.alert("Por favor, insira uma tarefa")
+    } else {
+        taskList.push(task.value.trim())
+        localStorage.setItem('taskList', JSON.stringify(taskList))
+    }
+    task.value = ''
+    updateList()
+}
+
+
+/* REMOVER UMA TAREFA */
+
+function removeTask(index) {
+    taskList.splice(index, 1)
+    localStorage.setItem('taskList', JSON.stringify(taskList))
+    updateList()
+}
